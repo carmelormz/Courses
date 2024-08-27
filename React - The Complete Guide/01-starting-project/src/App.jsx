812c6222b9f7b5bd47e64c828298f98329d01792
Carmelo Ramirez <jcarmelo37@gmail.com>
2024-08-26 17:56:02 -0700

    [Section 3][React Essentials] Added initial CoreConcept, Header, and TabButton components.

--- React - The Complete Guide/01-starting-project/src/App.jsx
+++ React - The Complete Guide/01-starting-project/src/App.jsx
@@ -1,16 +1,29 @@
+import { CORE_CONCEPTS } from "./data.js";
+
+import Header from "./components/Header/Header.jsx";
+import CoreConcept from "./components/CoreConcept/CoreConcept.jsx";
+import TabButton from './components/TabButton/TabButton.jsx';
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            {CORE_CONCEPTS.map((concept) => <CoreConcept {...concept}/>)}
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
+        </section>
       </main>
     </div>
   );
