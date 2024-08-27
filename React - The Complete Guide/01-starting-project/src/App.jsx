2726d3513fcec4297a20115fe89f1008e616741f
Carmelo Ramirez <jcarmelo37@gmail.com>
2024-08-26 19:12:48 -0700

    [Section 3][React Essentials] Added logic to conditionally show tab content based on tab clicked.

--- React - The Complete Guide/01-starting-project/src/App.jsx
+++ React - The Complete Guide/01-starting-project/src/App.jsx
@@ -6,10 +6,25 @@ import CoreConcept from "./components/CoreConcept/CoreConcept.jsx";
 import TabButton from "./components/TabButton/TabButton.jsx";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState("components");
+  const [selectedTopic, setSelectedTopic] = useState();
+
   const clickHandler = (buttonId) => {
     setSelectedTopic(buttonId);
   };
+
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
 
   return (
     <div>
@@ -33,13 +48,7 @@ function App() {
             <TabButton onClick={() => clickHandler("props")}>Props</TabButton>
             <TabButton onClick={() => clickHandler("state")}>State</TabButton>
           </menu>
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>{EXAMPLES[selectedTopic].code}</code>
-            </pre>
-          </div>
+          {tabContent}
         </section>
       </main>
     </div>
