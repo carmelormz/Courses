0109425b8039cda42ce85ae7b3460ee0186d4406
Carmelo Ramirez <jcarmelo37@gmail.com>
2024-08-27 21:40:20 -0700

    [Section 4][React Essentials - Deep Dive] Add new Tabs component and refactor code to include it.

--- React - The Complete Guide/01-starting-project/src/components/Examples/Examples.jsx
+++ React - The Complete Guide/01-starting-project/src/components/Examples/Examples.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import TabButton from "../TabButton/TabButton.jsx";
 import Section from "../Section/Section.jsx";
+import Tabs from "../Tabs/Tabs.jsx";
 
 import { EXAMPLES } from "../../data.js";
 
@@ -30,35 +31,35 @@ export default function Examples() {
     );
   }
 
+  const tabButtons = (
+    <>
+      <TabButton
+        onClick={() => clickHandler("components")}
+        isActive={isActive("components")}
+      >
+        Components
+      </TabButton>
+      <TabButton onClick={() => clickHandler("jsx")} isActive={isActive("jsx")}>
+        JSX
+      </TabButton>
+      <TabButton
+        onClick={() => clickHandler("props")}
+        isActive={isActive("props")}
+      >
+        Props
+      </TabButton>
+      <TabButton
+        onClick={() => clickHandler("state")}
+        isActive={isActive("state")}
+      >
+        State
+      </TabButton>
+    </>
+  );
+
   return (
     <Section title="Example" id="examples">
-      <menu>
-        <TabButton
-          onClick={() => clickHandler("components")}
-          isActive={isActive("components")}
-        >
-          Components
-        </TabButton>
-        <TabButton
-          onClick={() => clickHandler("jsx")}
-          isActive={isActive("jsx")}
-        >
-          JSX
-        </TabButton>
-        <TabButton
-          onClick={() => clickHandler("props")}
-          isActive={isActive("props")}
-        >
-          Props
-        </TabButton>
-        <TabButton
-          onClick={() => clickHandler("state")}
-          isActive={isActive("state")}
-        >
-          State
-        </TabButton>
-      </menu>
-      {tabContent}
+      <Tabs buttons={tabButtons}>{tabContent}</Tabs>
     </Section>
   );
 }
