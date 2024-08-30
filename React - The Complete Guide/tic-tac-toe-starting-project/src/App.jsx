f96a04e97b7f1154e507b73a2462f8280b34fb9b
Carmelo Ramirez <jcarmelo37@gmail.com>
2024-08-29 21:46:18 -0700

    [Section 4][React Essentials - Deep Dive] Add logic to check for winner.

--- React - The Complete Guide/tic-tac-toe-starting-project/src/App.jsx
+++ React - The Complete Guide/tic-tac-toe-starting-project/src/App.jsx
@@ -4,6 +4,27 @@ import Player from './components/Player/Player';
 import GameBoard from './components/GameBoard/GameBoard';
 import Log from './components/Log/Log';
 
+import { WINNING_COMBINATIONS } from './winning-combinations.js';
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+const computeGameBoard = (board, turns) => {
+  let gameBoard = [...board.map((row) => [...row])];
+
+  for (const turn of turns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
+
+  return gameBoard;
+};
+
 const deriveActivePlayer = (gameTurns) => {
   let currentPlayer = 'X';
 
@@ -14,10 +35,39 @@ const deriveActivePlayer = (gameTurns) => {
   return currentPlayer;
 };
 
+const checkForWinner = (board) => {
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstCellSymbol = board[combination[0].row][combination[0].column];
+    const secondCellSymbol = board[combination[1].row][combination[1].column];
+    const thirdCellSymbol = board[combination[2].row][combination[2].column];
+
+    if (
+      firstCellSymbol &&
+      firstCellSymbol === secondCellSymbol &&
+      firstCellSymbol === thirdCellSymbol
+    ) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
 
   const activePlayer = deriveActivePlayer(gameTurns);
+
+  let gameBoard = initialGameBoard;
+  gameBoard = computeGameBoard(gameBoard, gameTurns);
+
+  let winner = null;
+
+  if (checkForWinner(gameBoard)) {
+    winner = gameTurns[0].player;
+  }
+
+  console.log();
 
   const handleSelectSquare = (row, col) => {
     setGameTurns((prevTurns) => {
@@ -53,7 +103,8 @@ function App() {
             isActive={activePlayer === 'O'}
           />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+        {winner && <p>Player {winner} won!</p>}
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
     </main>
