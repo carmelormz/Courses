a5c13d33d8ffe534d5feecac22cd9a0132cb7e7d
Carmelo Ramirez <jcarmelo37@gmail.com>
2024-08-29 17:03:48 -0700

    [Section 4][React Essentials - Deep Dive] Add Player component.

---
+++ React - The Complete Guide/tic-tac-toe-starting-project/src/components/Player/Player.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const clickHandler = () => {
+    // Since we are updating a react state based on the state old/previous value
+    // is recommended to update by passing a function that sets the desired updated value
+    setIsEditing((wasEditing) => !wasEditing);
+  };
+
+  const handleInputChange = (e) => {
+    setPlayerName(e.target.value);
+  };
+
+  let playerNameEl = <span className='player-name'>{playerName}</span>;
+
+  if (isEditing) {
+    playerNameEl = (
+      <input
+        type='text'
+        required
+        value={playerName}
+        onChange={handleInputChange}
+      />
+    );
+  }
+
+  return (
+    <li>
+      <span className='player'>
+        {playerNameEl}
+        <span className='player-symbol'>{symbol}</span>
+      </span>
+      <button onClick={clickHandler}>{isEditing ? 'Save' : 'Edit'}</button>
+    </li>
+  );
+}
