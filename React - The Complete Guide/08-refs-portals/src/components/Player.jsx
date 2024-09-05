df21d8985821cdd09012b068ba43b536fa7cc38e
Carmelo Ramirez <jcarmelo37@gmail.com>
2024-09-05 12:36:16 -0700

    [Section 8][Refs & Portals] Update Player to use Refs to get values from text input.

--- React - The Complete Guide/08-refs-portals/src/components/Player.jsx
+++ React - The Complete Guide/08-refs-portals/src/components/Player.jsx
@@ -1,10 +1,20 @@
+import { useState, useRef } from 'react';
+
 export default function Player() {
+  const input = useRef();
+  const [playerName, setPlayerName] = useState(null);
+
+  const handleClick = () => {
+    setPlayerName(input.current.value);
+    input.current.value = '';
+  };
+
   return (
-    <section id="player">
-      <h2>Welcome unknown entity</h2>
+    <section id='player'>
+      <h2>Welcome {playerName ?? 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={input} type='text' />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
