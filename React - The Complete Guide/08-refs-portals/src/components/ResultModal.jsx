21adf66c4bfe99da5e44e8e3070478acbcc062d8
Carmelo Ramirez <jcarmelo37@gmail.com>
2024-09-20 19:40:54 -0700

    [Section 8][Refs & Portals] Update to use forward refs on inputs and timers

---
+++ React - The Complete Guide/08-refs-portals/src/components/ResultModal.jsx
@@ -0,0 +1,18 @@
+import { forwardRef } from 'react';
+
+export const ResultModal = forwardRef(({ result, targetTime }, ref) => {
+  return (
+    <dialog ref={ref} className='result-modal'>
+      <h2>You {result}!</h2>
+      <p>
+        The target time was <strong>{targetTime} seconds</strong>.
+      </p>
+      <p>
+        You stopped the timer with <strong>X seconds left</strong>.
+      </p>
+      <form action='dialog'>
+        <button>Close</button>
+      </form>
+    </dialog>
+  );
+});
