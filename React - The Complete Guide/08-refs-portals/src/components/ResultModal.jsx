2f49800082e19945a1be92a24d6ce7291a9c92ca
Carmelo Ramirez <jcarmelo37@gmail.com>
2024-09-20 20:23:33 -0700

    [Section 8][Refs & Portals] Update TimerChallenge and ResultModal to display winning message with score

--- React - The Complete Guide/08-refs-portals/src/components/ResultModal.jsx
+++ React - The Complete Guide/08-refs-portals/src/components/ResultModal.jsx
@@ -1,31 +1,39 @@
 import { forwardRef, useImperativeHandle, useRef } from 'react';
 
-export const ResultModal = forwardRef(({ result, targetTime }, ref) => {
-  const dialogRef = useRef();
+export const ResultModal = forwardRef(
+  ({ result, targetTime, remainingTime, onReset }, ref) => {
+    const dialogRef = useRef();
 
-  // Exposes internal methods and properties to outside components.
-  // Requires a forwarded ref (which is the one in which the method
-  // will be called.)
-  useImperativeHandle(ref, () => {
-    return {
-      open: () => {
-        dialogRef.current.showModal();
-      },
-    };
-  });
+    // Exposes internal methods and properties to outside components.
+    // Requires a forwarded ref (which is the one in which the method
+    // will be called.)
+    useImperativeHandle(ref, () => {
+      return {
+        open: () => {
+          dialogRef.current.showModal();
+        },
+      };
+    });
 
-  return (
-    <dialog ref={dialogRef} className='result-modal'>
-      <h2>You {result}!</h2>
-      <p>
-        The target time was <strong>{targetTime} seconds</strong>.
-      </p>
-      <p>
-        You stopped the timer with <strong>X seconds left</strong>.
-      </p>
-      <form action='dialog'>
-        <button>Close</button>
-      </form>
-    </dialog>
-  );
-});
+    const userLost = remainingTime <= 0;
+    const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+    const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
+
+    return (
+      <dialog ref={dialogRef} className='result-modal' onClose={onReset}>
+        {userLost && <h2>You Lost!</h2>}
+        {!userLost && <h2>Your Score: {score}</h2>}
+        <p>
+          The target time was <strong>{targetTime} seconds</strong>.
+        </p>
+        <p>
+          You stopped the timer with{' '}
+          <strong>{formattedRemainingTime} seconds left</strong>.
+        </p>
+        <form action='dialog' onSubmit={onReset}>
+          <button>Close</button>
+        </form>
+      </dialog>
+    );
+  }
+);
