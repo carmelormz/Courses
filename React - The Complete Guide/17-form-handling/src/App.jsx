b4875fa007f5d79d029255a414ffcefc5bc4a438
Carmelo Ramirez <jcarmelo37@gmail.com>
2025-01-14 21:52:52 -0800

    [Section 17][Form Handling] Add formdata-based form.

--- React - The Complete Guide/17-form-handling/src/App.jsx
+++ React - The Complete Guide/17-form-handling/src/App.jsx
@@ -1,13 +1,14 @@
 import Header from './components/Header.jsx';
 import LoginUsingRef from './components/LoginUsingRef.jsx';
 import LoginUsingState from './components/LoginUsingState.jsx';
+import Signup from './components/Signup.jsx';
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        <LoginUsingRef />
+        <Signup />
       </main>
     </>
   );
