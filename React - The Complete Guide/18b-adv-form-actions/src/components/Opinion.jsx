7c6a024605584c23056f592dce3f8631e4dbac70
Carmelo Ramirez <jcarmelo37@gmail.com>
2025-01-16 22:07:38 -0800

    [Section 18b][Adv Form Actions] Add logic to disable elements and optimistic updates while the form is being submitted.

--- React - The Complete Guide/18b-adv-form-actions/src/components/Opinion.jsx
+++ React - The Complete Guide/18b-adv-form-actions/src/components/Opinion.jsx
@@ -1,4 +1,31 @@
+import { useContext, useActionState, useOptimistic } from 'react';
+import { OpinionsContext } from '../store/opinions-context';
+
 export function Opinion({ opinion: { id, title, body, userName, votes } }) {
+  const { upvoteOpinion, downvoteOpinion } = useContext(OpinionsContext);
+
+  const [optimisticVotes, setVotesOptimistically] = useOptimistic(
+    votes,
+    (prevVotes, mode) => (mode === 'up' ? prevVotes + 1 : prevVotes - 1)
+  );
+
+  const upvoteAction = async () => {
+    setVotesOptimistically('up');
+    await upvoteOpinion(id);
+  };
+
+  const downvoteAction = async () => {
+    setVotesOptimistically('down');
+    await downvoteOpinion(id);
+  };
+
+  const [upvoteFormState, upvoteFormAction, upvotePending] =
+    useActionState(upvoteAction);
+  const [downvoteFormState, downvoteFormAction, downvotePending] =
+    useActionState(downvoteAction);
+
+  const disableButtons = upvotePending || downvotePending;
+
   return (
     <article>
       <header>
@@ -6,42 +33,42 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
         <p>Shared by {userName}</p>
       </header>
       <p>{body}</p>
-      <form className="votes">
-        <button>
+      <form className='votes'>
+        <button formAction={upvoteFormAction} disabled={disableButtons}>
           <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
+            xmlns='http://www.w3.org/2000/svg'
+            width='24'
+            height='24'
+            viewBox='0 0 24 24'
+            fill='none'
+            stroke='currentColor'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
           >
-            <rect width="18" height="18" x="3" y="3" rx="2" />
-            <path d="m16 12-4-4-4 4" />
-            <path d="M12 16V8" />
+            <rect width='18' height='18' x='3' y='3' rx='2' />
+            <path d='m16 12-4-4-4 4' />
+            <path d='M12 16V8' />
           </svg>
         </button>
 
-        <span>{votes}</span>
+        <span>{optimisticVotes}</span>
 
-        <button>
+        <button formAction={downvoteFormAction} disabled={disableButtons}>
           <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
+            xmlns='http://www.w3.org/2000/svg'
+            width='24'
+            height='24'
+            viewBox='0 0 24 24'
+            fill='none'
+            stroke='currentColor'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
           >
-            <rect width="18" height="18" x="3" y="3" rx="2" />
-            <path d="M12 8v8" />
-            <path d="m8 12 4 4 4-4" />
+            <rect width='18' height='18' x='3' y='3' rx='2' />
+            <path d='M12 8v8' />
+            <path d='m8 12 4 4 4-4' />
           </svg>
         </button>
       </form>
