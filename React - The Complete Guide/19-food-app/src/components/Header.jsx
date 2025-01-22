037ced7b3b5206dd87f2527eae5e3ad5ed2b9dbe
Carmelo Ramirez <jcarmelo37@gmail.com>
2025-01-21 21:05:46 -0800

    [Section 19][Food App] Finish CartContextProvider to initialize values and share state to consumers components.

--- React - The Complete Guide/19-food-app/src/components/Header.jsx
+++ React - The Complete Guide/19-food-app/src/components/Header.jsx
@@ -1,7 +1,13 @@
+import { useContext } from 'react';
 import logoImg from '../assets/logo.jpg';
 import Button from './UI/Button';
+import CartContext from '../store/CartContext';
 
 export default function Header() {
+  const { items } = useContext(CartContext);
+
+  const cartCount = items.reduce((total, item) => total + item.quantity, 0);
+
   return (
     <header id='main-header'>
       <div id='title'>
@@ -9,7 +15,7 @@ export default function Header() {
         <h1>FoodApp</h1>
       </div>
       <nav>
-        <Button textOnly>Cart (0)</Button>
+        <Button textOnly>Cart ({cartCount})</Button>
       </nav>
     </header>
   );
--- React - The Complete Guide/19-food-app/src/store/CartContext.jsx
+++ React - The Complete Guide/19-food-app/src/store/CartContext.jsx
@@ -29,15 +29,49 @@ function cartReducer(state, action) {
   }
 
   if (action.type === 'REMOVE_ITEM') {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+
+    const existingItem = state.items[existingCartItemIndex];
+
+    const updatedItems = [...state.items];
+
+    if (existingItem.quantity > 1) {
+      const updatedItem = {
+        ...existingItem,
+        quantity: existingItem.quantity - 1,
+      };
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems.splice(existingCartItemIndex, 1);
+    }
+
+    return { ...state, items: updatedItems };
   }
 
   return state;
 }
 
 export function CartContextProvider({ children }) {
-  const [] = useReducer(cartReducer, { items: [] });
+  const [cart, dispatchCartAction] = useReducer(cartReducer, { items: [] });
+
+  const addItem = (item) => {
+    dispatchCartAction({ type: 'ADD_ITEM', item });
+  };
+
+  const removeItem = (id) => {
+    dispatchCartAction({ type: 'REMOVE_ITEM', id });
+  };
+
+  const cartContext = {
+    items: cart.items,
+    addItem,
+    removeItem: () => {},
+  };
+
   // < React v19 => <CartContext.Provider></CartContext.Provider>
-  return <CartContext>{children}</CartContext>;
+  return <CartContext value={cartContext}>{children}</CartContext>;
 }
 
 export default CartContext;
