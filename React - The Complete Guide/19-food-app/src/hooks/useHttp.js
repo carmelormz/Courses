c5bc6df575226caeb3a2271dd4be945f6934f457
Carmelo Ramirez <jcarmelo37@gmail.com>
2025-01-23 18:18:10 -0800

    [Section 19][Food App] Create custom hook to manage http requests.

---
+++ React - The Complete Guide/19-food-app/src/hooks/useHttp.js
@@ -0,0 +1,46 @@
+import { useEffect, useState, useCallback } from 'react';
+
+const sendHttpRequest = async (url, config) => {
+  const response = await fetch(url, config);
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(
+      data.message || 'Something went wrong, failed to send request'
+    );
+  }
+
+  return data;
+};
+
+export default function useHttp(url, config, initialValue) {
+  const [data, setData] = useState(initialValue);
+  const [error, setError] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const sendRequest = useCallback(async () => {
+    setIsLoading(true);
+    try {
+      const resData = await sendHttpRequest(url, config);
+      setData(resData);
+    } catch (err) {
+      setError(err.message || 'Something went wrong.');
+    }
+    setIsLoading(false);
+  }, [url, config]);
+
+  // This useEffect purpose is to make the http request without the user manual call
+  // , only for GET methods.
+  useEffect(() => {
+    if ((config && (config.method === 'GET' || !config.method)) || !config) {
+      sendRequest();
+    }
+  }, [sendRequest, config]);
+
+  return {
+    data,
+    isLoading,
+    error,
+    sendRequest,
+  };
+}
