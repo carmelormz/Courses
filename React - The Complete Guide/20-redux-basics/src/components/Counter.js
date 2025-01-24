f7fc954d344d53c6d8b6454a5d65d7207fecc804
Carmelo Ramirez <jcarmelo37@gmail.com>
2025-01-24 12:40:59 -0800

    [Section 20][React Basics] Add logic to incremenet counter by x amount.

--- React - The Complete Guide/20-redux-basics/src/components/Counter.js
+++ React - The Complete Guide/20-redux-basics/src/components/Counter.js
@@ -4,6 +4,7 @@ import { ACTION_TYPES } from '../store';
 
 const Counter = () => {
   const counter = useSelector((state) => state.counter);
+  const showCounter = useSelector((state) => state.showCounter);
   const dispatch = useDispatch();
 
   const handleIncrement = () => {
@@ -14,14 +15,21 @@ const Counter = () => {
     dispatch({ type: ACTION_TYPES.DECREMENT });
   };
 
-  const toggleCounterHandler = () => {};
+  const handleIncrease = () => {
+    dispatch({ type: ACTION_TYPES.INCREASE, amount: 5 });
+  };
+
+  const toggleCounterHandler = () => {
+    dispatch({ type: ACTION_TYPES.TOGGLE_COUNTER });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {showCounter && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={handleIncrement}>Increment</button>
+        <button onClick={handleIncrease}>Increase by 5</button>
         <button onClick={handleDecrement}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
--- React - The Complete Guide/20-redux-basics/src/store/index.js
+++ React - The Complete Guide/20-redux-basics/src/store/index.js
@@ -2,11 +2,14 @@ import { createStore } from 'redux';
 
 const STORE_COUNTER_INITIAL_VALUE = {
   counter: 0,
+  showCounter: true,
 };
 
 export const ACTION_TYPES = {
   INCREMENT: 'increment',
   DECREMENT: 'decrement',
+  INCREASE: 'increase',
+  TOGGLE_COUNTER: 'toggle_counter',
 };
 
 const counterReducer = (state = STORE_COUNTER_INITIAL_VALUE, action) => {
@@ -16,6 +19,14 @@ const counterReducer = (state = STORE_COUNTER_INITIAL_VALUE, action) => {
 
   if (action.type === ACTION_TYPES.DECREMENT) {
     return { ...state, counter: state.counter - 1 };
+  }
+
+  if (action.type === ACTION_TYPES.INCREASE) {
+    return { ...state, counter: state.counter + action.amount };
+  }
+
+  if (action.type === ACTION_TYPES.TOGGLE_COUNTER) {
+    return { ...state, showCounter: !state.showCounter };
   }
 
   return state;
