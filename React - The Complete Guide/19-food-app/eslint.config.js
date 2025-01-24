73e1702278b390ef93bb96d5c64fed436713f7da
Carmelo Ramirez <jcarmelo37@gmail.com>
2025-01-23 18:57:36 -0800

    [Section 19][Food App] Add logic to handle loading and error states in meals component.

---
+++ React - The Complete Guide/19-food-app/eslint.config.js
@@ -0,0 +1,19 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    rules: {
+      'no-unused-vars': 'warn',
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat['jsx-runtime'],
+];
