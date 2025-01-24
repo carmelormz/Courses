0b05055340a67c66d21beec0a3067886eab22c8c
Carmelo Ramirez <jcarmelo37@gmail.com>
2025-01-24 11:37:17 -0800

    [Section 20][Redux Demo] Create small redux demo to test basic features of redux.

---
+++ React - The Complete Guide/20-redux-basic-demo/redux-demo.js
@@ -0,0 +1,24 @@
+const redux = require('redux');
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === 'INCREMENT') {
+    return { ...state, counter: state.counter + 1 };
+  }
+
+  if (action.type === 'DECREMENT') {
+    return { ...state, counter: state.counter - 1 };
+  }
+  return state;
+};
+
+const store = redux.createStore(counterReducer);
+
+const counterSubscriber = () => {
+  const state = store.getState();
+  console.log(state);
+};
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({ type: 'INCREMENT' });
+store.dispatch({ type: 'DECREMENT' });
