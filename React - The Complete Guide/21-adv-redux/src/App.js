1b90854c495952e02ff6820f64dc1d41cd582a89
Carmelo Ramirez <jcarmelo37@gmail.com>
2025-01-28 19:41:29 -0800

    [Section 21][Adv Redux] Add new Notification component and logic to store cart on Firebase DB when its updated.

--- React - The Complete Guide/21-adv-redux/src/App.js
+++ React - The Complete Guide/21-adv-redux/src/App.js
@@ -1,16 +1,77 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import { useEffect } from 'react';
+import { showNotification } from './store/slices/uiSlice';
+import Notification from './components/UI/Notification';
+
+let isInitial = true;
 
 function App() {
   const showCart = useSelector((state) => state.ui.showCart);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const sendCartData = async () => {
+      dispatch(
+        showNotification({
+          status: 'pending',
+          title: 'Sending...',
+          message: 'Sending cart data.',
+        })
+      );
+
+      const response = await fetch(
+        'https://react-complete-guide-dem-751a5-default-rtdb.firebaseio.com/cart.json',
+        { method: 'PUT', body: JSON.stringify(cart) }
+      );
+
+      if (!response.ok) {
+        throw new Error('Sending cart data failed');
+      }
+
+      dispatch(
+        showNotification({
+          status: 'success',
+          title: 'Success!',
+          message: 'Sent cart data successfully.',
+        })
+      );
+    };
+
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    sendCartData().catch((error) => {
+      dispatch(
+        showNotification({
+          status: 'error',
+          title: 'Error!',
+          message: error.message,
+        })
+      );
+    });
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 }
 
--- React - The Complete Guide/21-adv-redux/src/store/slices/uiSlice.js
+++ React - The Complete Guide/21-adv-redux/src/store/slices/uiSlice.js
@@ -2,6 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const UI_SLICE_INITIAL_STATE = {
   showCart: false,
+  notification: null,
 };
 
 const uiSlice = createSlice({
@@ -11,9 +12,16 @@ const uiSlice = createSlice({
     toggleCart: (state) => {
       state.showCart = !state.showCart;
     },
+    showNotification: (state, action) => {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
+    },
   },
 });
 
-export const { toggleCart } = uiSlice.actions;
+export const { toggleCart, showNotification } = uiSlice.actions;
 
 export default uiSlice.reducer;
