7f966cfc50b83b86f874370fed4045ba347c2606
Carmelo Ramirez <jcarmelo37@gmail.com>
2025-01-29 19:51:33 -0800

    [Section 22][React Router] Added basic routing using ReactRouter.

--- React - The Complete Guide/22-react-router/src/App.js
+++ React - The Complete Guide/22-react-router/src/App.js
@@ -1,5 +1,30 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import HomePage from './pages/Home';
+import ProductsPage from './pages/Products';
+import RootLayout from './pages/Root';
+import ErrorPage from './pages/Error';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      {
+        path: '/',
+        element: <HomePage />,
+      },
+      {
+        path: '/products',
+        element: <ProductsPage />,
+      },
+    ],
+    errorElement: <ErrorPage />,
+  },
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
