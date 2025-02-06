71065b829a205f361bc897aae9468bda1af72693
Carmelo Ramirez <jcarmelo37@gmail.com>
2025-02-05 19:59:14 -0800

    [Section 22][React Router] Add action to new event page to submit form.

--- React - The Complete Guide/22-react-router-p2/frontend/src/pages/NewEvent.js
+++ React - The Complete Guide/22-react-router-p2/frontend/src/pages/NewEvent.js
@@ -1,3 +1,37 @@
+import { redirect } from 'react-router-dom';
+import EventForm from '../components/EventForm';
+
 export default function NewEventPage() {
-  return <h1>New Event Page</h1>;
+  return <EventForm />;
 }
+
+export const action = async ({ request, params }) => {
+  const data = await request.formData();
+
+  // Ex. const title = data.get('title');
+  const eventData = {
+    title: data.get('title'),
+    image: data.get('image'),
+    date: data.get('date'),
+    description: data.get('description'),
+  };
+
+  const response = await fetch('http://localhost:8080/events', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(eventData),
+  });
+
+  if (!response.ok) {
+    throw new Response(
+      JSON.stringify({
+        message: 'Could not save event.',
+      }),
+      {
+        status: 500,
+      }
+    );
+  }
+
+  return redirect('/events');
+};
