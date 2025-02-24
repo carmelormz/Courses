59bead6f7e1e33564aec156bae4dbd846698d5c1
Carmelo Ramirez <jcarmelo37@gmail.com>
2025-02-24 13:38:27 -0800

    [Section 24][Deployment] Add lazy loading for pages and loaders.

--- React - The Complete Guide/24-deployment/src/App.js
+++ React - The Complete Guide/24-deployment/src/App.js
@@ -1,9 +1,13 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import BlogPage, { loader as postsLoader } from './pages/Blog';
 import HomePage from './pages/Home';
-import PostPage, { loader as postLoader } from './pages/Post';
+// import PostPage, { loader as postLoader } from './pages/Post';
 import RootLayout from './pages/Root';
+import { lazy, Suspense } from 'react';
+
+const BlogPage = lazy(() => import('./pages/Blog'));
+
+const PostPage = lazy(() => import('./pages/Post'));
 
 const router = createBrowserRouter([
   {
@@ -17,8 +21,30 @@ const router = createBrowserRouter([
       {
         path: 'posts',
         children: [
-          { index: true, element: <BlogPage />, loader: postsLoader },
-          { path: ':id', element: <PostPage />, loader: postLoader },
+          {
+            index: true,
+            element: (
+              <Suspense fallback={<p>Loading...</p>}>
+                <BlogPage />
+              </Suspense>
+            ),
+            loader: async () => {
+              const module = await import('./pages/Blog');
+              return module.loader();
+            },
+          },
+          {
+            path: ':id',
+            element: (
+              <Suspense fallback={<p>Loading...</p>}>
+                <PostPage />
+              </Suspense>
+            ),
+            loader: async (meta) => {
+              const module = await import('./pages/Post');
+              return module.loader(meta);
+            },
+          },
         ],
       },
     ],
