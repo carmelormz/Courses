daa4d36c6133f2c311fcefaf1f448276db22c063
Carmelo Ramirez <jcarmelo37@gmail.com>
2025-06-02 19:43:15 -0700

    [Section 26][NextJS] Add logic to display data for meal items in Meals Details Page.

--- React - The Complete Guide/26-nextjs/foodies-project/app/meals/[mealSlug]/page.js
+++ React - The Complete Guide/26-nextjs/foodies-project/app/meals/[mealSlug]/page.js
@@ -1,9 +1,40 @@
+import Image from 'next/image';
+import classes from './page.module.css';
+import { getMeal } from '@/lib/meals';
+import { notFound } from 'next/navigation';
+
 export default async function MealDetailsPage({ params }) {
   const { mealSlug } = await params;
+  const meal = await getMeal(mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, '<br />');
+
   return (
-    <main>
-      <h1>Meals Details</h1>
-      <p>{mealSlug}</p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} fill alt={meal.title} />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a hfref={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
   );
 }
