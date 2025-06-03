e9b174f5d9a33d2292656fe9dc032c122cb3d147
Carmelo Ramirez <jcarmelo37@gmail.com>
2025-06-02 20:03:10 -0700

    [Section 26][NextJS] Improve ImagePicker to show a preview of the selected image.

--- React - The Complete Guide/26-nextjs/foodies-project/components/meals/image-picker.js
+++ React - The Complete Guide/26-nextjs/foodies-project/components/meals/image-picker.js
@@ -1,19 +1,48 @@
 'use client';
 
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import classes from './image-picker.module.css';
+import Image from 'next/image';
 
 export default function ImagePicker({ label, name }) {
   const inputRef = useRef();
+  const [pickedImage, setPickedImage] = useState();
 
   const handlePickerClick = () => {
     inputRef.current.click();
+  };
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+
+    if (!file) {
+      setPickedImage(null);
+      return;
+    }
+
+    const fileReader = new FileReader();
+    // Needs to be set before loading image, in order to get the result url.
+    fileReader.onload = () => {
+      const fileUrl = fileReader.result;
+      setPickedImage(fileUrl);
+    };
+    fileReader.readAsDataURL(file);
   };
 
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!pickedImage && <p>No image selected...</p>}
+          {pickedImage && (
+            <Image
+              src={pickedImage}
+              alt='The image selected by the user'
+              fill
+            />
+          )}
+        </div>
         <input
           ref={inputRef}
           className={classes.input}
@@ -21,6 +50,8 @@ export default function ImagePicker({ label, name }) {
           id={name}
           accept='image/png, image/jpeg'
           name={name}
+          onChange={handleImageChange}
+          required
         />
         <button
           className={classes.button}
