47effc35cbb4c2eb0fb6ba3eab91b5179083f532
Carmelo Ramirez <jcarmelo37@gmail.com>
2025-06-06 19:56:28 -0700

    [Section 28][React Animations] Add animation via Framer Motion to modal component to make it disappear when closing.

--- React - The Complete Guide/28-react-animations/src/components/Header.jsx
+++ React - The Complete Guide/28-react-animations/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-
+import { AnimatePresence } from 'framer-motion';
 import NewChallenge from './NewChallenge.jsx';
 
 export default function Header() {
@@ -15,11 +15,13 @@ export default function Header() {
 
   return (
     <>
-      {isCreatingNewChallenge && <NewChallenge onDone={handleDone} />}
+      <AnimatePresence>
+        {isCreatingNewChallenge && <NewChallenge onDone={handleDone} />}
+      </AnimatePresence>
 
-      <header id="main-header">
+      <header id='main-header'>
         <h1>Your Challenges</h1>
-        <button onClick={handleStartAddNewChallenge} className="button">
+        <button onClick={handleStartAddNewChallenge} className='button'>
           Add Challenge
         </button>
       </header>
