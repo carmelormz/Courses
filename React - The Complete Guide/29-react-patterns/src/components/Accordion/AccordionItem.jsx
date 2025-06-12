e56998e634a685f6f19612b9cdb1fbe090289040
Carmelo Ramirez <jcarmelo37@gmail.com>
2025-06-11 21:54:53 -0700

    [Section 29][React Components] Create new AccordionTitle and AccordionContent component to improve reusability and configurability.

--- React - The Complete Guide/29-react-patterns/src/components/Accordion/AccordionItem.jsx
+++ React - The Complete Guide/29-react-patterns/src/components/Accordion/AccordionItem.jsx
@@ -1,17 +1,3 @@
-import { use } from 'react';
-import { useAccordionContext } from './Accordion';
-
-export default function AccordionItem({ id, className, title, children }) {
-  const { openItemId, toggleItem } = useAccordionContext();
-
-  const isOpen = openItemId === id;
-
-  return (
-    <li className={className}>
-      <h3 onClick={() => toggleItem(id)}>{title}</h3>
-      <div className={`accordion-item-content ${isOpen ? 'open' : undefined}`}>
-        {children}
-      </div>
-    </li>
-  );
+export default function AccordionItem({ className, children }) {
+  return <li className={className}>{children}</li>;
 }
